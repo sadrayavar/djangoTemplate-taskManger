244a75475105bdafd3fae14055c7cba29ac79a41
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2023-11-22 10:21:11 +0330

    edit: removed zahra's test commit

--- taskManager/urls.py
+++ taskManager/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
-urlpatterns = [cb
+urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.homePage),
     path('user/',include('user.urls'))
