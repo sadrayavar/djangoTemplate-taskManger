8d77d386a7a5e6aee6cf4f8bde96dd5e5a963a75
zahra <zahrarostami137878@gmail.com>
2023-11-23 17:43:36 +0330

    create users information model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+Class userInfo(models.model):
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
