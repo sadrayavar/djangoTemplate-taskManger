3ef7aca2ad985f9de1a6b7c32a01d400404d66d7
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2023-11-23 22:16:23 +0330

    add: login_required in views add: login template

--- taskManager/views.py
+++ taskManager/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 
 # Create your views here.
+@login_required(login_url="/user/login/")
 def homePage(request):
     return render(request, "home.html")
