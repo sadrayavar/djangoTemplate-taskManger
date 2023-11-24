651a87fb86bafb3b071007c5667c7f449547dfc7
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2023-11-24 21:38:10 +0330

    add: delete and edit task edit: convert ' to " add: messages

--- task/urls.py
+++ task/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.taskList, name="taskList"),
     path("<int:taskId>/", views.singleTask, name="singleTask"),
+    path("<int:taskId>/delete", views.deleteTask, name="deleteTask"),
+    path("<int:taskId>/edit", views.editTask, name="editTask"),
     path("add/", views.addTask, name="addTask"),
 ]
--- task/views.py
+++ task/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .forms import TaskForm
 from .models import Task
-from django.contrib.auth.decorators import login_required
 
 
-@login_required(login_url="/account/login/")
+# @login_required(login_url="/account/login/")
 def addTask(request):
     if request.method == "POST":
         form = TaskForm(request.POST)
@@ -18,14 +19,40 @@ def addTask(request):
     return render(request, "taskForm.html", {"form": form})
 
 
-@login_required(login_url="/accounts/login/")
+# @login_required(login_url="/accounts/login/")
 def taskList(request):
     tasks = Task.objects.all()
-    print(tasks)
     return render(request, "taskList.html", {"tasks": tasks})
 
 
-@login_required(login_url="/accounts/login/")
+# @login_required(login_url="/accounts/login/")
 def singleTask(request, taskId):
     taskDetail = Task.objects.get(id=taskId)
     return render(request, "singleTask.html", {"task": taskDetail})
+
+
+# @login_required(login_url="/accounts/login/")
+def deleteTask(request, taskId):
+    Task.objects.get(id=taskId).delete()
+    messages.success(
+        request,
+        f"To do with id of {taskId} is deleted succesfully.",
+        extra_tags="success",
+    )
+    return redirect("taskList")
+
+
+# @login_required(login_url="/accounts/login/")
+def editTask(request, taskId):
+    task = Task.objects.get(id=taskId)
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task.save()
+            return redirect(".")
+    else:
+        print(task)
+        print(task.title)
+        form = TaskForm(request.POST, instance=task)
+        print(form)
+        return render(request, "taskForm.html", {"form": form})
