801fb5262c7540e735d5311f05afd98574a46a6d
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2023-11-24 13:36:58 +0330

    add: task model

--- task/models.py
+++ task/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    STATE_CHOICES = [
+        ("New", "New"),
+        ("In Progress", "In Progress"),
+        ("Completed", "Completed"),
+    ]
+
+    title = models.CharField(max_length=50)
+    priority = models.IntegerField()
+    state = models.CharField(max_length=11, choices=STATE_CHOICES)
+    deadline = models.DateTimeField()
