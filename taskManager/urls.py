e2a56e2d503d78cced2639cac7eb6925cd664166
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2023-12-01 16:27:01 +0330

    add: file uploading to tasks

--- taskManager/urls.py
+++ taskManager/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from comment.views import myComments
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -10,3 +12,6 @@ urlpatterns = [
     path("comments/", myComments, name="myCommentsPage"),
     path("search/", views.search, name="searchPage"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
