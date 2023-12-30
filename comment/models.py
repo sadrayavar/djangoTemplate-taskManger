669a4b1339c9327665fa02a1132b11c02aea69e7
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2023-12-30 20:25:54 +0330

    add: hidden attribute to comment model

--- comment/models.py
+++ comment/models.py
@@ -9,11 +9,7 @@ class Comment(models.Model):
     text = models.TextField(max_length=1000)
     date = models.DateField(auto_now_add=True)
     time = models.TimeField(auto_now_add=True)
+    hidden = models.BooleanField(default=False)
 
-    @property
-    def datetime(self):
-        return datetime.combine(self.date, self.time)
-
-    # replyTo = models.OneToOneField(Comment, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
