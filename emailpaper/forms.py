4eda7c932d0920e66934efb38803a826af880fb2
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2024-01-22 22:38:59 +0330

    add footer to admin page

--- emailpaper/forms.py
+++ emailpaper/forms.py
@@ -4,7 +4,7 @@ from .models import Newspaper
 
 class NewspaperForm(forms.ModelForm):
     email = forms.EmailField(
-        label="Enter your email to add it to the newspaper List. Add again to remove it.",
+        label="Enter your email to add it to the newspaper List (Add again to remove it).",
         widget=forms.EmailInput(
             attrs={
                 "class": "form-control form-control-dark text-bg-light",
