3048673693bbadf858b4669b3f7e30621befbaef
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2024-01-23 20:42:26 +0330

    add: task approvement to admin_console remove: "include with" syntax

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.adminHomePage, name="adminPage"),
     path("hide_commmment/<int:commentId>", views.hideComment, name="hideComment"),
+    path("approve_task/<int:taskId>", views.approveTask, name="approveTask"),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponseForbidden
+from task.models import Task
 from taskManager.constant import generateBasicData, logo, taskTitles, dynamicTabs
 from comment.models import Comment
 
@@ -7,11 +8,14 @@ from comment.models import Comment
 def adminHomePage(request):
     if request.user.is_superuser:
         comments = Comment.objects.all()
+        tasks = Task.objects.filter(approved=False)
 
         data = {
             "adminConsole": True,
             "comments": comments,
             "commentsCount": len(comments),
+            "tasks": tasks,
+            "tasksCount": len(tasks),
         }
         context = generateBasicData(request, "adminPage", "admin")
         return render(request, "adminPage.html", {**context, **data})
@@ -24,6 +28,16 @@ def hideComment(request, commentId):
         comment = Comment.objects.get(id=commentId)
         comment.hidden = True if comment.hidden == False else False
         comment.save()
+        return redirect("adminPage")
+    else:
+        return HttpResponseForbidden()
+
+
+def approveTask(request, taskId):
+    if request.user.is_superuser:
+        task = Task.objects.get(id=taskId)
+        task.approved = True if task.approved == False else False
+        task.save()
         return redirect("adminPage")
     else:
         return HttpResponseForbidden()
